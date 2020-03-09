3f3c1a8daf47b1272d67135d837bc7a4380a364c
s18891 <s18891@pjwstk.edu.pl>
2020-03-09 17:00:41 +0100

    commit3

---
+++ ConsoleApp1/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1.Models
+{
+    public class Student
+    {
+        [XmlElement(ElementName = "InneNazwa2")]
+        public string Imie { get; set; }
+
+        [XmlAttribute(AttributeName = "InnaNazwa")]
+        public string Nazwisko { get; set; }
+
+        [XmlAttribute(AttributeName = "InnaNazwa3")]
+        public string Email { get; set; }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace ConsoleApp1
 {
@@ -9,7 +13,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
 
-            string path = @"Data\dane.csv";
+            string path = @"Dane\dane.csv";
 
             //Wczytywanie
 
@@ -20,12 +24,39 @@ namespace ConsoleApp1
 
             while ((line = stream.ReadLine()) != null)
             {
-                Concole.WriteLine(line);
+                Console.WriteLine(line);
             }
+        
 
-            stream.Dispose();
+            //stream.Dispose();
+
+
+            //XML
+
+
+            var list = new List<Student>();
+            var st = new Student
+            {
+                Imie ="Jan",
+                Nazwisko="Kowalski",
+                Email="kowalski@wp.pl"
+
+
+            };
+            list.Add(st);
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
+                new XmlRootAttribute("uczelnia"));
+
+            serializer.Serialize(writer, list);
+            writer.Dispose();
+
+
+
 
         }
+
 
     }
 }
