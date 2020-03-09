40f9e53c718d1258798ae7a6812a57d17e4a976a
s18891 <s18891@pjwstk.edu.pl>
2020-03-09 16:22:58 +0100

    2 commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,25 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            string path = @"Data\dane.csv";
+
+            //Wczytywanie
+
+            var fi = new FileInfo(path);
+            var stream = new StreamReader(fi.OpenRead());
+
+            string line = null;
+
+            while ((line = stream.ReadLine()) != null)
+            {
+                Concole.WriteLine(line);
+            }
+
+            stream.Dispose();
+
         }
+
     }
 }
