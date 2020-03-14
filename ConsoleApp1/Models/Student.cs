bec9cb23b9cdfcc0240b4feb748129bfd22b707d
Bogusław Stępień s18891 <s18891@pjwstk.edu.pl>
2020-03-14 23:21:46 +0100

    Revert "wykonana podstawowa funkcjonalność, bez obsługi błędów itp."
    This reverts commit 94cc7533

--- ConsoleApp1/Models/Student.cs
+++ ConsoleApp1/Models/Student.cs
@@ -7,36 +7,14 @@ namespace ConsoleApp1.Models
 {
     public class Student
     {
-        [XmlAttribute(AttributeName = "indexNumber")]
-        
-        public string numerstudenta { get; set; }
-
-        
-        [XmlElement(ElementName = "fname")]
+        [XmlElement(ElementName = "InneNazwa2")]
         public string Imie { get; set; }
 
-        [XmlElement(ElementName = "lname")]
+        [XmlAttribute(AttributeName = "InnaNazwa")]
         public string Nazwisko { get; set; }
-        
-        [XmlElement(ElementName = "birthdate")]
-        public string dataUrodzenia { get; set; }
-        
-        [XmlElement(ElementName = "email")]
-        public string email { get; set; }
-        
-        [XmlElement(ElementName = "mothersname")]
-        public string imieMatki { get; set; }
-        
-        [XmlElement(ElementName = "fathersname")]
-        public string imieOjca { get; set; }
-     
-        //[XmlElement(ElementName = "studies")] /// nie wiem jak zrobić nowy znacznik
-        
-        [XmlElement(ElementName = "name")]
-        public string kierunekStudiow { get; set; }
-        
-        [XmlElement(ElementName = "mode")]
-        public string typStudiow { get; set; }
+
+        [XmlAttribute(AttributeName = "InnaNazwa3")]
+        public string Email { get; set; }
 
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,27 +21,11 @@ namespace ConsoleApp1
             var stream = new StreamReader(fi.OpenRead());
 
             string line = null;
-            string[] splitLine = null;
-            var list = new List<Student>();
-
-
 
             while ((line = stream.ReadLine()) != null)
             {
                 Console.WriteLine(line);
-                
-                splitLine = line.Split(',');
-
-
-                if (splitLine.Length != 9)
-                {
-                    Console.WriteLine("ERROR");
-                }
-            
-
-
-
-            
+            }
         
 
             //stream.Dispose();
@@ -50,23 +34,17 @@ namespace ConsoleApp1
             //XML
 
 
+            var list = new List<Student>();
             var st = new Student
             {
-                Imie =splitLine[0],
-                Nazwisko=splitLine[1],
-               kierunekStudiow = splitLine[2],
-               typStudiow = splitLine[3],
-               numerstudenta = splitLine[4],
-               dataUrodzenia = splitLine[5],
-               email = splitLine[6],
-               imieMatki = splitLine[7],
-               imieOjca = splitLine[8],
-               
+                Imie ="Jan",
+                Nazwisko="Kowalski",
+                Email="kowalski@wp.pl"
 
 
             };
             list.Add(st);
-            }
+
             FileStream writer = new FileStream(@"data.xml", FileMode.Create);
             XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
                 new XmlRootAttribute("uczelnia"));
