222fbef0c7f3eb007b29d0dded483543e3fc1fae
s18891 <s18891@pjwstk.edu.pl>
2020-03-14 23:58:10 +0100

    wykonana podstawowa funkcjonalność, bez obsługi błędów(v1)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,37 +19,54 @@ namespace ConsoleApp1
 
             var fi = new FileInfo(path);
             var stream = new StreamReader(fi.OpenRead());
-            
+
             string line = null;
+            string[] splitLine = null;
+            var list = new List<Student>();
+
+
 
             while ((line = stream.ReadLine()) != null)
             {
                 Console.WriteLine(line);
-            }
+                
+                splitLine = line.Split(',');
+
+
+                if (splitLine.Length != 9)
+                {
+                    Console.WriteLine("ERROR");
+                }
+            
+
+
+
+            
         
 
-            //stream.Dispose();
+                //stream.Dispose();
 
 
-            //XML
+                //XML
 
 
-            var list = new List<Student>();
-            var st = new Student
-            {
-                Imie ="Jan",
-                Nazwisko="Kowalski",
-                Email="kowalski@wp.pl"
-                
-                
-                
-                
-                
+                var st = new Student
+                {
+                    Imie =splitLine[0],
+                    Nazwisko=splitLine[1],
+                    kierunekStudiow = splitLine[2],
+                    typStudiow = splitLine[3],
+                    numerstudenta = splitLine[4],
+                    dataUrodzenia = splitLine[5],
+                    email = splitLine[6],
+                    imieMatki = splitLine[7],
+                    imieOjca = splitLine[8],
+               
 
 
-            };
-            list.Add(st);
-
+                };
+                list.Add(st);
+            }
             FileStream writer = new FileStream(@"data.xml", FileMode.Create);
             XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
                 new XmlRootAttribute("uczelnia"));
