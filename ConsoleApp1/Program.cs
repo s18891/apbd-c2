ae5f588a8b14ee795076b82f9ffe09c483f369a6
s18891 <s18891@pjwstk.edu.pl>
2020-03-15 00:50:32 +0100

    dodanie obsługi błędów: błędna ścieżka pliku, błędna ilość kolumn w tym pliku

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,73 +10,104 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            
+            string pathCsv = @"Dane\dane.csv";
+            string pathXml = @"data.xml";
 
-            string path = @"Dane\dane.csv";
+            if (args.Length==3)
+            {
+                            Console.WriteLine(args[0]);
+                            Console.WriteLine(args[1]);
+                            Console.WriteLine(args[2]);
+                            pathCsv = args[0];
+                            pathXml = args[1];
+
+
+
+            }
 
             //Wczytywanie
-
-            var fi = new FileInfo(path);
-            var stream = new StreamReader(fi.OpenRead());
-
-            string line = null;
-            string[] splitLine = null;
-            var list = new List<Student>();
-
-
-
-            while ((line = stream.ReadLine()) != null)
+            try
             {
-                Console.WriteLine(line);
-                
-                splitLine = line.Split(',');
 
-
-                if (splitLine.Length != 9)
-                {
-                    Console.WriteLine("ERROR");
-                }
-            
-
-
-
-            
-        
-
-                //stream.Dispose();
-
-
-                //XML
-
-
-                var st = new Student
-                {
-                    Imie =splitLine[0],
-                    Nazwisko=splitLine[1],
-                    kierunekStudiow = splitLine[2],
-                    typStudiow = splitLine[3],
-                    numerstudenta = splitLine[4],
-                    dataUrodzenia = splitLine[5],
-                    email = splitLine[6],
-                    imieMatki = splitLine[7],
-                    imieOjca = splitLine[8],
-               
-
-
-                };
-                list.Add(st);
             }
-            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
-                new XmlRootAttribute("uczelnia"));
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+            var fi = new FileInfo(pathCsv);
+            if (!fi.Exists)
+            {
+                File.AppendAllText(@"log.txt", "Błędna ścieżka pliku źródłowego"+Environment.NewLine);
 
-            serializer.Serialize(writer, list);
-            writer.Dispose();
+            }
+            else
+            {
+
+                var stream = new StreamReader(fi.OpenRead());
+
+                string line = null;
+                string[] splitLine = null;
+                var list = new List<Student>();
+
+
+
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+
+                    splitLine = line.Split(',');
+
+
+                    if (splitLine.Length != 9)
+                    {
+                        Console.WriteLine("ERROR");
+                        File.AppendAllText(@"log.txt", "Błędna ilośc kolumn w wierszu w pliku wejściowym"+Environment.NewLine);
+
+                    }
 
 
 
 
+
+
+
+                    //stream.Dispose();
+
+
+                    //XML
+
+
+                    var st = new Student
+                    {
+                        Imie = splitLine[0],
+                        Nazwisko = splitLine[1],
+                        kierunekStudiow = splitLine[2],
+                        typStudiow = splitLine[3],
+                        numerstudenta = splitLine[4],
+                        dataUrodzenia = splitLine[5],
+                        email = splitLine[6],
+                        imieMatki = splitLine[7],
+                        imieOjca = splitLine[8],
+
+
+
+                    };
+                    list.Add(st);
+                }
+
+                FileStream writer = new FileStream(pathXml, FileMode.Create);
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
+                    new XmlRootAttribute("uczelnia"));
+
+                serializer.Serialize(writer, list);
+                writer.Dispose();
+
+
+
+            }
         }
 
 
